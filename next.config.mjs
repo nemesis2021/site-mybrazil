cf2613b709abcafb40502ab3acd7526076b44fef
dugudu <166384961+dugudu@users.noreply.github.com>
2025-09-22 04:00:04 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,1 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  images: {
-    unoptimized: true,
-  },
-}
-
-export default nextConfig
+/** @type {import('next').NextConfig} */ const nextConfig = { output: 'export', eslint: { ignoreDuringBuilds: true, }, typescript: { ignoreBuildErrors: true, }, images: { unoptimized: true, }, } export default nextConfig
